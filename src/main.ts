e09c74b127559396e5881b235a10d9c929bcc048
Casper Ndlovu <casperndlovu42@gmail.com>
2023-07-17 19:32:55 +0200

    chore: refactored main

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,59 @@
-import Copier from './Copier.js';
-import { IDestination } from './IDestination.js';
-import { ISource } from './ISource.js';
-import { mock, instance } from 'ts-mockito';
+import Copier from "./Copier.js";
+import { IDestination } from "./IDestination.js";
+import { ISource } from "./ISource.js";
+import { mock, instance } from "ts-mockito";
 
-export const copyCharacter = (character?: string) => {
+let source: ISource;
+let destination: IDestination;
 
-  // Create mocks
+export const initSource = () => {
+  // Create Source Mocks
   const mockedSource: ISource = mock<ISource>();
+  // Mock Source Instance
+  const source: ISource = instance(mockedSource);
+
+  source.ReadChar = readCharacter;
+
+  return source;
+};
+
+export const initDestination = () => {
+  // Create Destination Mocks
   const mockedDestination: IDestination = mock<IDestination>();
 
-  // Mock Source
-  const source: ISource = instance(mockedSource);
-  source.character = character || 'Super\nman\n';
-
-  source.ReadChar = () =>
-    source.character.toString().length > 1 &&
-    !/\n/g.test(source.character.toString().substring(0, 1))
-      ? source.character.toString().charAt(0)
-      : null;
-
-  // Mock Destination
+  // Mock Destination Instance
   const destination: IDestination = instance(mockedDestination);
-  mockedDestination.character = '';
 
-  destination.WriteChar = (char: string) => {
-    mockedDestination.character += new String(char);
-  };
+  destination.WriteChar = writeCharacter;
 
+  return destination;
+};
+
+export const readCharacter = () =>
+  source?.character?.toString()?.length > 1 &&
+  !/\n/g.test(source?.character?.toString()?.substring(0, 1))
+    ? source.character.toString().charAt(0)
+    : null;
+
+export const writeCharacter = (char: string) => {
+  destination.character += new String(char);
+};
+
+export const copyCharacter = (character: string = 'Super\nman\n') => {
+  source = initSource();
+  destination = initDestination();
+
+  source.character = character;
+  destination.character = '';
+
+  // Init Copier
   const copier: Copier = new Copier(source, destination);
+
+  // Copy from ISource to IDestination
   copier.Copy();
 
-  console.log('Destination character: ', mockedDestination.character);
-  return mockedDestination.character;
+  console.log("Destination character: ", destination?.character);
+  return destination?.character;
 };
 
 export default copyCharacter();
