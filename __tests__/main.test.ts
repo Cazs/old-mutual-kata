0638e845be1ef47f5694ca6b5245316267aaeb5f
Casper Ndlovu <casperndlovu42@gmail.com>
2023-07-18 02:27:33 +0200

    fix: refactored main module for the bonus piece

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,23 +1,33 @@
-import { copyCharacter, copyCharacters } from '../src/main.js';
+import { copyCharacter, copyCharacters } from "../src/main.js";
 
-describe('Old Mutual Code Kata Unit Tests', () => {
-    it('Should expect the response to be Batman', () => {
-        expect(copyCharacter('Batman\nSuperman')).toBe('Batman');
-    });
-    
-    it('Should expect the response to be Super', () => {
-        expect(copyCharacter()).toBe('Super');
-    });
-    
-    it('Should expect the response to be Ironman', () => {
-        expect(copyCharacter('Ironman\n')).toBe('Ironman');
-    });
-    
-    it('Should expect the response to be empty', () => {
-        expect(copyCharacter('\nSpiderman')).toBe('');
-    });
-    
-    it('Should expect something', () => {
-        copyCharacters('Spiderman');
-    });
+describe("Old Mutual Code Kata Unit Tests", () => {
+  it("Should expect the response to be Batman", () => {
+    process.env.OM_CHAR_NAME = "Batman\nSuperman";
+    process.env.OM_CHAR_NAME_ARBITRARY_LEN = "0";
+    expect(copyCharacter(process.env.OM_CHAR_NAME)).toBe("Batman");
+  });
+
+  it("Should expect the response to be Super", () => {
+    process.env.OM_CHAR_NAME = "Super\nman";
+    process.env.OM_CHAR_NAME_ARBITRARY_LEN = "0";
+    expect(copyCharacter()).toBe("Super");
+  });
+
+  it("Should expect the response to be Ironman", () => {
+    process.env.OM_CHAR_NAME = "Ironman\n";
+    process.env.OM_CHAR_NAME_ARBITRARY_LEN = "0";
+    expect(copyCharacter()).toBe("Ironman");
+  });
+
+  it("Should expect the response to be empty", () => {
+    process.env.OM_CHAR_NAME = "\nSpiderman";
+    process.env.OM_CHAR_NAME_ARBITRARY_LEN = "0";
+    expect(copyCharacter()).toBe("");
+  });
+
+  it("Should expect the response to be Spider", () => {
+    process.env.OM_CHAR_NAME = "Spider\nman";
+    process.env.OM_CHAR_NAME_ARBITRARY_LEN = "5";
+    expect(copyCharacters("Spider\nman")).toBe("Spider");
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Copier from './Copier.js';
-import { IDestination } from './IDestination.js';
-import { ISource } from './ISource.js';
-import { mock, instance } from 'ts-mockito';
+import Copier from "./Copier.js";
+import { IDestination } from "./IDestination.js";
+import { ISource } from "./ISource.js";
+import { mock, instance } from "ts-mockito";
 
 // Declare ISource Global Instance
 let source: ISource;
@@ -12,6 +12,7 @@ let destination: IDestination;
 // Declare Copier Global Instance
 let copier: Copier;
 
+// Instantiate Global ISource Mock Variable
 export const initSource = (): ISource => {
   // Create Source Mocks
   const mockedSource: ISource = mock<ISource>();
@@ -26,6 +27,7 @@ export const initSource = (): ISource => {
   return source;
 };
 
+// Instantiate Global IDestination Mock Variable
 export const initDestination = (): IDestination => {
   // Create Destination Mocks
   const mockedDestination: IDestination = mock<IDestination>();
@@ -40,35 +42,41 @@ export const initDestination = (): IDestination => {
   return destination;
 };
 
-// ISource Stubs
+// ISource ReadChar Stub
 export const readCharacter = (): string | undefined => {
-  const minPos = /\n/g.test(source?.character?.toString()) ? 1 : 0;
-
-  return source?.character?.toString()?.length > minPos &&
-  !/\n/g.test(source?.character?.toString()?.substring(minPos, 1))
+  return source?.character?.toString()?.length > 1 &&
+    !/\n/g.test(source?.character?.toString()?.substring(0, 1))
     ? source.character.toString().charAt(0)
     : undefined;
-}
+};
 
+// ISource ReadChar Bonus Stub
 export const readCharacters = (count: number): string[] => {
   const charLen = source?.character?.toString()?.length;
 
-  const newLineDelimeterPos = source?.character?.toString()?.indexOf('\n');
+  const newLineDelimeterPos = source?.character?.toString()?.indexOf("\n");
 
   let maxLength = count >= 0 && count < charLen ? count : charLen;
   maxLength = newLineDelimeterPos > maxLength ? newLineDelimeterPos : maxLength;
 
-  return source?.character?.toString()?.substring(0, maxLength)?.split('');
+  return source?.character?.toString()?.substring(0, maxLength)?.split("");
 };
 
-// IDestination Stubs
+// IDestination WriteChar Stub
 export const writeCharacter = (char: string) => {
   destination.character += char;
-  console.log('> IDestination Current Character Name: ', destination?.character);
+  console.log(
+    "> IDestination Current Character Name: ",
+    destination?.character,
+    " <"
+  );
 };
 
-export const writeCharacters = (chars: string[]) => writeCharacter(chars.join(''));
+// IDestination WriteChar Bonus Stub
+export const writeCharacters = (chars: string[]) =>
+  writeCharacter(chars.join(""));
 
+// Instantiate Global Variables
 export const initGlobals = (character: string) => {
   // Init ISource and IDestination
   source = initSource();
@@ -76,29 +84,52 @@ export const initGlobals = (character: string) => {
 
   // Init defaults
   source.character = character;
-  destination.character = '';
+  destination.character = "";
 
   // Init Copier
   copier = new Copier(source, destination);
-}
+};
 
+// Execute Phase One Of The Kata
 export const copyCharacter = (character?: string) => {
-  initGlobals(character || process.env.OM_CHAR_NAME || 'Super\nman\n');
+  const c =
+    character?.toString() ||
+    process.env?.OM_CHAR_NAME?.toString() ||
+    "Super\nman\n";
+  initGlobals(c);
 
   // Copy from ISource to IDestination
   copier.Copy();
 
-  console.log('\n>>> IDestination Final Character Name: ', destination?.character, ' <<<');
+  console.log(
+    "\n>>> IDestination Final Character Name: ",
+    destination?.character,
+    " <<<"
+  );
+
   return destination?.character;
 };
 
+// Execute Phase Two (Bonus) Of The Kata
 export const copyCharacters = (character?: string) => {
-  initGlobals(character || process.env.OM_CHAR_NAME || 'Super\nman\n');
+  const c =
+    character?.toString() ||
+    process.env?.OM_CHAR_NAME?.toString() ||
+    "Super\nman\n";
+  initGlobals(c);
 
-  writeCharacters(readCharacters(Number(process.env.OM_CHAR_NAME_ARBITRARY_LEN || '0')));
+  writeCharacters(
+    readCharacters(Number(process.env?.OM_CHAR_NAME_ARBITRARY_LEN || "0"))
+  );
 
-  console.log('\n>>> IDestination Final Character Name (Arbitrary Length): ', destination?.character, ' <<<');
+  console.log(
+    "\n>>> IDestination Final Character Name (Arbitrary Length): ",
+    destination?.character,
+    " <<<"
+  );
   return destination?.character;
 };
 
-export default Number(process.env.OM_CHAR_NAME_ARBITRARY_LEN || '0') > 0 ? copyCharacters() : copyCharacter();
+export default Number(process.env?.OM_CHAR_NAME_ARBITRARY_LEN || "0") > 0
+  ? copyCharacters()
+  : copyCharacter();
