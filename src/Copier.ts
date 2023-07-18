f1c3bc11ac88113944354b342901e7bf2e9c2e39
Casper Ndlovu <casperndlovu42@gmail.com>
2023-07-18 07:01:23 +0200

    fix: restore source character

--- src/Copier.ts
+++ src/Copier.ts
@@ -11,11 +11,15 @@ export default class Copier {
   }
 
   Copy(): void {
+    const tempSourceChar: string = new String(this._source.character).toString();
+
     while (this._source.ReadChar()) {
       this._destination.WriteChar(this._source.ReadChar().toString());
       this._source.character = this._source.character
         .toString()
         .substring(1, this._source.character.toString().length);
     }
+    
+    this._source.character = tempSourceChar;
   }
 }
